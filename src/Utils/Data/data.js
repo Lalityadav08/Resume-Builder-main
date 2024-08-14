0df346b3df7cbc1c22d1424f8140c31eb4f7168a
Lalityadav08 <rohityaduvanshi8055@gmail.com>
2024-08-14 16:33:58 +0530

    first commit

---
+++ src/Utils/Data/data.js
@@ -0,0 +1,47 @@
+//Dummy Data for testing purpose
+
+export const data = {
+  personal_info: {
+    first_name: "Lalit",
+    last_name: "Yadav",
+    email: "rohityaduvanshi8055@gmail.com",
+    mobile: "0123456789",
+    address: "Shyam Nagar, Etawah, UP",
+    city: "Etawah",
+    state: "Uttar-Pradesh",
+    postal_code: "206001",
+    objective:
+      "To learn develop applications  with knowlege of MERN stack development at ALMABETTER",
+  },
+  work_experience: [
+    {
+      id: 1,
+      job_title: "Recruit Manager",
+      organization_name: "HP Internship India",
+      start_year: "2017",
+      end_year: "2023",
+    },
+    {
+      id: 2,
+      job_title: "Customer Relationship Manager",
+      organization_name: "Alma Better",
+      start_year: "2017",
+      end_year: "2022",
+    },
+    {
+      id: 3,
+      job_title: "Sales Manager",
+      organization_name: "Alma X",
+      start_year: "2022",
+      end_year: "2023",
+    },
+  ],
+  education_details: {
+    type: "Graduation",
+    university: "JUIT Solan",
+    degree: "B.Tech",
+    start_year: "2019",
+    end_year: "2023",
+  },
+  key_skills: ["JAVA", "AWS","Angular","MERN Stack", "Swift", "Python"],
+};
